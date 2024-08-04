abe5b819198b683275714fc4d02fa84e0da857e6
Ridhi Batra <119876916+RidhiBatra@users.noreply.github.com>
2024-08-05 00:25:15 +0530

    Add files via upload

---
+++ linkedlistques1.cpp
@@ -0,0 +1,15 @@
+/*Write a function to find the Decimal Value of a Number which is represented in Binary using a Linked List.
+
+Example: 1 -> 0 -> 1 -> 0 -> 1
+Output: 21
+
+
+
+
+
+
+
+Write a function to bring all the ODD elements of a linked List Before Even Numbers without disturbing the order of EVEN numbers and ODD numbers
+Example: 2 -> 3 -> 8 -> 5 -> 1 
+
+Output: 3 -> 5 -> 1 -> 2 -> 8
